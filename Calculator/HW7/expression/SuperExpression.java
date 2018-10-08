9e614983b19e7ea9697983fd46ae0b1ddcbb863f
maxLundin <242440@niuitmo.ru>
2018-10-08 11:54:04 +0300

    message

--- Calculator/HW7/expression/SuperExpression.java
+++
@@ -1,4 +0,0 @@
-package expression;
-
-interface SuperExpression extends DoubleExpression, Expression {
-}
--- Calculator/HW7/expression/parser/Test.java
+++
@@ -1,9 +0,0 @@
-package expression.parser;
-
-
-public class Test {
-	public static void main(String args[]) throws ParserException, DivisionByZeroException, OverflowException {
-		ExpressionParser parser = new ExpressionParser();
-		System.out.println((parser.parse("log10 1").evaluate(0,0,-1249535276)));
-	}
-}
